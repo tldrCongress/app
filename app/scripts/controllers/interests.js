16b83b5cbb535836073548a6f69781b8843246f3
InertiaTechnology <steven@social-inertia.com>
2015-03-21 16:42:58 -0700

    Changed interests controller
    - Changed “interest” to “interests”
    - Added support for toggling which are selected
    - Function to load external json data

--- app/scripts/controllers/interests.js
+++ app/scripts/controllers/interests.js
@@ -7,9 +7,22 @@
  * # InterestCtrl
  * Controller of the hack4CongressApp
  */
-app.controller('InterestCtrl', function ($scope) {
+app.controller('InterestsCtrl', function ($scope, $http) {
 
-    $scope.intersts = {};
+    $scope.interests = [
+		{
+			'name' : 'Environment',
+			'support' : 1
+		},
+		{
+			'name' : 'Health',
+			'support' : 1
+		},
+		{
+			'name' : 'Pro Life',
+			'support' : 0
+		}
+	];
 
 	// Load the json data for intersts
 	$http.get('/data/intersts.json')
@@ -20,5 +33,12 @@ app.controller('InterestCtrl', function ($scope) {
         .error(function(data,status,error,config){
             $scope.contents = [{heading:"Error",description:"Could not load json   data"}];
 		});
+
+
+	// Updates the user's interest settings, i = interest number in array
+	$scope.toggleInterest = function(i)
+	{
+		$scope.interests[i].support = !$scope.interests[i].support;
+	}
 
 });
