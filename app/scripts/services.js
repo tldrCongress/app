16ee3c3b4801eba9f8e348178652733ddd063180
tiz <tizhao@gmail.com>
2015-03-22 12:17:51 -0700

    reference to our firebase inside services.js

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -3,6 +3,16 @@ angular.module('hack4CongressApp.services', ['firebase'])
 .factory('hack4CongressFirebase', function($firebase) {
   return new Firebase("https://blistering-inferno-7388.firebaseio.com/");
 })
+.factory('AllData', function($firebase, hack4CongressFirebase) {
+
+  // create an AngularFire reference to the data
+  var sync = $firebase(hack4CongressFirebase);
+
+  // download the data into a local object
+  var syncObject = sync.$asObject();
+
+  return syncObject;
+})
 .factory('Voter', function($firebase, hack4CongressFirebase) {
   var voterRoot = hack4CongressFirebase.child("Voters");
   return {
