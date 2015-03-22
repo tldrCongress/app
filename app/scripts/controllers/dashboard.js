5c12b4cb2a7dc1fa3742323c77d53b095641b40c
InertiaTechnology <steven@social-inertia.com>
2015-03-21 19:51:18 -0700

    Included state icons, updates to dashboard

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -10,15 +10,24 @@
 app.controller('DashboardCtrl', function ($scope, $http) {
 
 	$scope.rep = {};
+	$scope.votes = {};
 
 	// Load the json data for intersts
 	$http.get('/data/profile.json')
-		.success(function(data) {
-			console.log(data);
-			$scope.rep = data;
+		.success(function(d) {
+			$scope.rep = d;
 		})
 		.error(function(data, status, error, config){
 			$scope.rep = [{heading:"Error",description:"Could not load json   data"}];
 		});
 
+
+	// Load the voting record
+	$http.get('/data/record.json')
+		.success(function(d) {
+			$scope.votes = d;
+		})
+		.error(function(data, status, error, config){
+			$scope.votes = [{heading:"Error",description:"Could not load json   data"}];
+		});
 });
