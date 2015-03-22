a056797f912aa2428a01948fe68b65f61a5c0ea2
Jimmy Hsu <jimmyhsu@live.com>
2015-03-22 13:42:23 -0700

    Add reason for vote

---
+++ dist/scripts/controllers/interests.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hack4CongressApp.controller:InterestCtrl
+ * @description
+ * # InterestCtrl
+ * Controller of the hack4CongressApp
+ */
+app.controller('InterestsCtrl', function ($scope, $http, $firebase, Interests, VoterInterests, Voter ) {
+
+    var vid = 'v001'; // HARD CODED FOR NOW
+    // $scope.voter = Voter.get(vid);
+    // $scope.interests = Interests.get();
+
+    // TODO: GET RID OF THIS
+    // Load the json data for intersts
+    $http.get('/data/interests.json')
+        .success(function(data) {
+            $scope.interests = data;
+        })
+        .error(function(data, status, error, config){
+            $scope.interests = [{heading:"Error",description:"Could not load json   data"}];
+     });
+
+    // Updates the user's interest settings, i = interest number in array
+    $scope.toggleInterest = function(i)
+    {
+        $scope.interests[i].support = !$scope.interests[i].support;
+        // VoterInterests.save(vid, $scope.interests);
+    }
+
+});
---
+++ dist/scripts/controllers/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hack4CongressApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hack4CongressApp
+ */
+app.controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
