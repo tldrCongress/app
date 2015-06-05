45aa0c1abdd14713a34130eca80cee8869009c64
tiz <tizhao@gmail.com>
2015-06-05 14:54:09 -0700

    style tweaks to login.js

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,23 +8,22 @@
  * Controller of the hack4CongressApp
  */
 app.controller('LoginCtrl', ['$scope', '$location', '$http', 'Auth',
-	function ($scope, $location, $http, Auth) {
-		$scope.login = function () {
-      Auth.$authWithPassword({
-        email: $scope.email,
-        password: $scope.password
-      }).then(function(authData) {
-        $scope.authData = authData;
-        console.log(authData)
-        $location.path('/stafferDashboard');
-      }, function(error) {
-        if (error = 'INVALID_EMAIL') {
-          alert('email invalid');
-        } else if (error = 'INVALID_PASSWORD') {
-          alert('wrong password!');
-        } else {
-          alert('Oops, something went wrong :(');
-        }
-      });
+  function ($scope, $location, $http, Auth) {
+  $scope.login = function () {
+    Auth.$authWithPassword({
+    email: $scope.email,
+    password: $scope.password
+    }).then(function(authData) {
+    $scope.authData = authData;
+    $location.path('/stafferDashboard');
+    }, function(error) {
+    if (error = 'INVALID_EMAIL') {
+      alert('email invalid');
+    } else if (error = 'INVALID_PASSWORD') {
+      alert('wrong password!');
+    } else {
+      alert('Oops, something went wrong :(');
     }
+    });
+  }
 }]);
